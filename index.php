ce089f240534bd9ea812de43acbb60cc0772d711
DiegoDuenez <19170154@uttcampus.edu.mx>
2023-01-28 11:39:16 -0600

    init

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    
+    <?php include 'templates/header.html'; ?>
+
+</head>
+<body>
+
+    <?php include 'templates/layouts/index.html'; ?>
+
+    <?php include 'templates/scripts.html'; ?>
+
+    
+</body>
+</html>
